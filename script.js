d34829f276ad9cbdf4a9bb532ffcefbb47298e18
James Butler <jamesedjoe@gmail.com>
2021-03-29 22:57:24 +0100

    updated comments

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
+// shape classes
 const X_CLASS = 'x'
 const CIRCLE_CLASS = 'circle'
+
+// game winning combinations
 const WINNING_COMBINATIONS = [
   [0, 1, 2],
   [3, 4, 5],
@@ -10,6 +13,7 @@ const WINNING_COMBINATIONS = [
   [0, 4, 8],
   [2, 4, 6]
 ]
+// game-board elements
 const cellElements = document.querySelectorAll('[data-cell]')
 const board = document.getElementById('board')
 const winningMessageElement = document.getElementById('winningMessage')
@@ -17,15 +21,18 @@ const restartButton = document.getElementById('restartButton')
 const winningMessageTextElement = document.querySelector('[data-winning-message-text]')
 let circleTurn
 
+// score counters
 var tieCounter = 0;
 var xCounter = 0;
 var oCounter = 0;
 
+// function call to run & restart game
 startGame()
-
 restartButton.addEventListener('click', startGame)
 
+// main game function
 function startGame() {
+  // game starts with X but O will start if their score is lower
   if (xCounter > oCounter) {
     circleTurn = true;
     document.getElementById("indicator").innerHTML = "O Starts";
@@ -34,6 +41,8 @@ function startGame() {
     circleTurn = false;
     document.getElementById("indicator").innerHTML = "X Starts";
   }
+
+  // game-board setup
   cellElements.forEach(cell => {
     cell.classList.remove(X_CLASS)
     cell.classList.remove(CIRCLE_CLASS)
@@ -44,6 +53,7 @@ function startGame() {
   winningMessageElement.classList.remove('show')
 }
 
+// handles game input & checks if game won
 function handleClick(e) {
   const cell = e.target
   const currentClass = circleTurn ? CIRCLE_CLASS : X_CLASS
@@ -58,6 +68,7 @@ function handleClick(e) {
   }
 }
 
+// when called ends game & displays end game message + updates counters
 function endGame(draw) {
   if (draw) {
     winningMessageTextElement.innerText = 'Tie!'
@@ -78,16 +89,19 @@ function endGame(draw) {
   winningMessageElement.classList.add('show')
 }
 
+// checks if game is a tie
 function isDraw() {
   return [...cellElements].every(cell => {
     return cell.classList.contains(X_CLASS) || cell.classList.contains(CIRCLE_CLASS)
   })
 }
 
+// function to place "game piece"
 function placeMark(cell, currentClass) {
   cell.classList.add(currentClass)
 }
 
+// alternates turns & updates indicator
 function swapTurns() {
   if (circleTurn) {
     document.getElementById("indicator").innerHTML = "X's Turn";
@@ -98,6 +112,7 @@ function swapTurns() {
   circleTurn = !circleTurn
 }
 
+// for highlighting game pieces before placement
 function setBoardHoverClass() {
   board.classList.remove(X_CLASS)
   board.classList.remove(CIRCLE_CLASS)
@@ -108,6 +123,7 @@ function setBoardHoverClass() {
   }
 }
 
+// cross checks winning combinations with current board
 function checkWin(currentClass) {
   return WINNING_COMBINATIONS.some(combination => {
     return combination.every(index => {
@@ -116,6 +132,7 @@ function checkWin(currentClass) {
   })
 }
 
+// switches between light & dark depending on selector choice or local storage
 function applyTheme(theme) {
   document.body.classList.remove("theme-light", "theme-dark");
   document.body.classList.add(`theme-${theme}`);
@@ -135,4 +152,3 @@ document.addEventListener("DOMContentLoaded", () => {
     applyTheme(this.value);
   });
 });
-
